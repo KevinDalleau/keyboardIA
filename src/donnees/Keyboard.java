587bfd591d72587fa1fde76022591172e12f365f
elmakhzo1u <driss.el-makhzoumi@telecomnancy.eu>
2016-01-06 11:43:21 +0100

    erratum

--- src/donnees/Keyboard.java
+++ src/donnees/Keyboard.java
@@ -4,15 +4,15 @@ import java.util.Arrays;
 
 public class Keyboard {
 	private char[] clavier;
-	
+
 	public Keyboard(){
 		this.clavier = new char[40];
 	}
-	
+
 	public void setCharAt(char a,int x){
 		this.clavier[x] = a;
 	}
-	
+
 	public void display(){
 		System.out.println("Disposition du clavier");
 		for(int i=0;i<4;i++){
@@ -22,7 +22,7 @@ public class Keyboard {
 			System.out.println("\n");
 		}
 	}
-	
+
 	public boolean swap(int x, int y){
 		boolean swapped = false;
 		if(this.clavier[x]!= ' ' || this.clavier[y]!=' '){
@@ -36,13 +36,13 @@ public class Keyboard {
 		}
 		return swapped;
 	}
-	
+
 
 	public int getLetter(int a) { //Get the letter at a given position (a) on the keyboard.
 		int index = Arrays.asList(this.clavier).indexOf(a);
 		return index;
 	}
-	
+
 	public double getCostBinary(int a, int b) { //Calculate the cost between two indexes of letters. a and b : positions on the 4*10 keyboard
 		Bigramme bigramme = new Bigramme();
 		int aLetter = this.getLetter(a);
@@ -50,7 +50,7 @@ public class Keyboard {
 		int freq = bigramme.frequence(aLetter, bLetter);
 		double distance = getDistance(a,b);
 		double cost = freq*distance;
-		
+
 		return cost;
 	}
 
@@ -58,10 +58,11 @@ public class Keyboard {
 		double distance = Math.sqrt((Math.pow((a-(a%10))/10,2))+Math.pow((b%10-a),2));
 		return distance;
 	}
-	
+
 	public void copy(Keyboard key){
 		for(int i = 0;i<key.clavier.length;i++){
 			this.clavier[i] = key.clavier[i];
 		}
-}
+	}
 
+}
