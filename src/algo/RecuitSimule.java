b9f4f6240d6c84e8b848dac021cfce0323fe0d5f
elmakhzo1u <driss.el-makhzoumi@telecomnancy.eu>
2016-01-08 05:15:21 +0100

    Fixing simulted annealing temperature decay

--- src/algo/RecuitSimule.java
+++ src/algo/RecuitSimule.java
@@ -7,15 +7,18 @@ public class RecuitSimule extends Algorithme{
 	private double Tmax;
 	private double Emax;
 	private double finalCost;
+	private double Alpha;
 	
 	public RecuitSimule(){
 		this.Tmax = 1000;
 		this.Emax = 100;
+		this.Alpha = 0.9;
 	}
 
-	public RecuitSimule(double tmax,double emax){
+	public RecuitSimule(double tmax,double emax, double alpha){
 		this.Tmax = tmax;
 		this.Emax = emax;
+		this.Alpha = alpha;
 	}
 
 	public void Compute(){
@@ -24,13 +27,11 @@ public class RecuitSimule extends Algorithme{
 		bestSol.copy(firstSol);
 		int energy = (int) firstSol.getCost();
 		int bestEnergy = energy;
-		int t = 0;
-		while(t<Tmax && energy>Emax ){
-			double T = t/Tmax;
+		while(1<Tmax && energy>Emax ){
 			Keyboard newkey = this.generateNeighbor(firstSol);
 			//newEnergy
 			int newEnergy = (int) newkey.getCost();
-			if(this.acceptanceProbability(energy, newEnergy, T)>Math.random()){
+			if(this.acceptanceProbability(energy, newEnergy, Tmax)>Math.random()){
 				firstSol.copy(newkey);
 				energy = newEnergy;
 			}
@@ -38,8 +39,8 @@ public class RecuitSimule extends Algorithme{
 				bestSol.copy(newkey);
 				bestEnergy = newEnergy;
 			}
-			t++;
-                        this.updateResultat(bestSol);
+			this.Tmax *= this.Alpha;
+         this.updateResultat(bestSol);
 		}
 	}
 
@@ -71,13 +72,23 @@ public class RecuitSimule extends Algorithme{
                 super.configure();
 		this.parametres.put("Temperature",1000.0);
 		this.parametres.put("Energy",0.0);
+		this.parametres.put("Decay", 0.9);
 	}
 
 	@Override
 	protected void launch() {
             this.setTmax((double)this.getParametre("Temperature"));
             this.setEmax((double)this.getParametre("Energy"));
+            this.setAlpha((double)this.getParametre("Decay"));
             this.Compute();
+	}
+
+	public double getAlpha() {
+		return Alpha;
+	}
+
+	public void setAlpha(double alpha) {
+		Alpha = alpha;
 	}
 
 	public double getTmax() {
